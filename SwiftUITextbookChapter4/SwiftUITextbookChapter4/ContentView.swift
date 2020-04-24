6d12ee60da6b8bf1b38839b6cfadc653caac9fa6
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2020-04-24 14:53:32 +0900

    Listを追加

---
+++ SwiftUITextbookChapter4/SwiftUITextbookChapter4/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUITextbookChapter4
+//
+//  Created by home on 2020/04/24.
+//  Copyright © 2020 Swift-beginners. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
