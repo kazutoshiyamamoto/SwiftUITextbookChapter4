9f9165f9ab05a52227f2d282582d777178eb7efe
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2020-04-24 14:55:27 +0900

    リストに6行分のテキストを追加

--- SwiftUITextbookChapter4/SwiftUITextbookChapter4/ContentView.swift
+++ SwiftUITextbookChapter4/SwiftUITextbookChapter4/ContentView.swift
@@ -10,7 +10,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        List {
+            Text("Content 1")
+            Text("Content 2")
+            Text("Content 3")
+            Text("Content 4")
+            Text("Content 5")
+            Text("Content 6")
+        }
     }
 }
 
