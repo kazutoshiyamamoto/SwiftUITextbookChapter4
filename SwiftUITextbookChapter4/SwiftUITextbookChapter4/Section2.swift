e4713cec5c6ffb968bae6166fa687bd88e7b814a
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2020-04-26 12:52:29 +0900

    Embed in Listでリスト表示を作る

---
+++ SwiftUITextbookChapter4/SwiftUITextbookChapter4/Section2.swift
@@ -0,0 +1,23 @@
+//
+//  Section4-2.swift
+//  SwiftUITextbookChapter4
+//
+//  Created by home on 2020/04/26.
+//  Copyright © 2020 Swift-beginners. All rights reserved.
+//
+
+import SwiftUI
+
+struct Section2: View {
+    var body: some View {
+        List(/*@START_MENU_TOKEN@*/0 ..< 5/*@END_MENU_TOKEN@*/) { item in
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        }
+    }
+}
+
+struct Section2_Previews: PreviewProvider {
+    static var previews: some View {
+        Section2()
+    }
+}
