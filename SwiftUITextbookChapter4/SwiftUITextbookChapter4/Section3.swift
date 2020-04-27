37c8cf118e3d95de825755d43cf5a42089609efd
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2020-04-27 14:10:48 +0900

    リストに表示する要素を格納した配列を追加

---
+++ SwiftUITextbookChapter4/SwiftUITextbookChapter4/Section3.swift
@@ -0,0 +1,24 @@
+//
+//  Section3.swift
+//  SwiftUITextbookChapter4
+//
+//  Created by home on 2020/04/27.
+//  Copyright © 2020 Swift-beginners. All rights reserved.
+//
+
+import SwiftUI
+
+struct Section3: View {
+    let shikoku = ["徳島県", "香川県", "愛媛県", "高知県"]
+    let kyusyu = ["福岡県", "佐賀県", "長崎県", "熊本県", "大分県", "宮崎県", "鹿児島県"]
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Section3_Previews: PreviewProvider {
+    static var previews: some View {
+        Section3()
+    }
+}
