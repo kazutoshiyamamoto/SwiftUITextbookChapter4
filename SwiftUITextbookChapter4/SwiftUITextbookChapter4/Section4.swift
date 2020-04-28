e033de601481bec5d96c2a189172603c529ca5db
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2020-04-28 14:16:59 +0900

    写真データを扱うファイルを追加

---
+++ SwiftUITextbookChapter4/SwiftUITextbookChapter4/Section4.swift
@@ -0,0 +1,21 @@
+//
+//  Section4.swift
+//  SwiftUITextbookChapter4
+//
+//  Created by home on 2020/04/28.
+//  Copyright © 2020 Swift-beginners. All rights reserved.
+//
+
+import SwiftUI
+
+struct Section4: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Section4_Previews: PreviewProvider {
+    static var previews: some View {
+        Section4()
+    }
+}
