6bea9360e95ca51fa268058e372b8229a4be87b7
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2020-05-04 23:45:01 +0900

    画像を表示

---
+++ SwiftUITextbookChapter4/SwiftUITextbookChapter4/Section5.swift
@@ -0,0 +1,29 @@
+//
+//  Section5.swift
+//  SwiftUITextbookChapter4
+//
+//  Created by home on 2020/05/04.
+//  Copyright © 2020 Swift-beginners. All rights reserved.
+//
+
+import SwiftUI
+
+struct Section5: View {
+    var body: some View {
+        NavigationView {
+            VStack(alignment: .leading, spacing: 5.0) {
+                Image("city")
+                .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(height: 200.0)
+                    .padding(.bottom)
+            }
+        }
+    }
+}
+
+struct Section5_Previews: PreviewProvider {
+    static var previews: some View {
+        Section5()
+    }
+}
